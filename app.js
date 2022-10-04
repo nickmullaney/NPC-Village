650d82d741b1fc5aa2f95d1c6ce0c476fc63d13c
Nick Mullaney <nick.mullaney@gmail.com>
2022-10-04 04:48:22 +0000

    the math works! It's Alive

---
+++ app.js
@@ -0,0 +1,42 @@
+function yourName(){
+let userName = prompt("What is your name Adventurer?")
+document.write ("Hello Adventurer " + userName + ", welcome to NPC Village")
+}
+
+function npcAge(){
+    let charAge = prompt("How old is this NPC?")
+        if(charAge > 1000){
+            console.log("That character is ancient!")
+            return charAge;
+        }
+        else if (charAge < 8){
+            console.log("Tis but a child!")
+            return charAge;
+        }
+        else if(charAge >0 || charAge <1000){
+            return charAge;
+        }
+        else{alert ("Let's try that again?")
+        charAge = npcAge();
+        return charAge;
+    }
+}
+
+function getNpcAge(){
+let charAge = npcAge();
+document.write ("Age: " + charAge)
+}
+
+function farewellName(){
+    let newUserName = prompt("What do we call you when you leave this place?")
+            document.write  ("Thanks for stopping by " + newUserName + " The Magnificent");
+}
+
+function characterAge() {
+                charAge = random();
+                console.log(charAge);
+            }
+            
+function random(){
+    return(Math.floor(Math.random() * 100));
+            }    
